02cb9ce07ed24b2d80be6f10c1bee48f80289c55
Stefan Darius <darius.stefan@opensips.org>
2026-05-27 12:22:11 +0300

    fix: tighten remark-heading-id type shim to Plugin<[], Root>

--- src/types/remark-heading-id.d.ts
+++ src/types/remark-heading-id.d.ts
@@ -1,5 +1,6 @@
 declare module 'remark-heading-id' {
+  import type { Root } from 'mdast';
   import type { Plugin } from 'unified';
-  const plugin: Plugin;
+  const plugin: Plugin<[], Root>;
   export default plugin;
 }
